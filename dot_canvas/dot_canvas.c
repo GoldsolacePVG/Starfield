0de9ab877ecb70549790a0c5342becb257911414
Pabolt84 <>
2024-12-28 12:50:17 +0100

    sectorizado

--- dot_canvas/dot_canvas.c
+++ dot_canvas/dot_canvas.c
@@ -16,7 +16,7 @@ static int cpu_mhz = 0;
 static int dump = 0;
 
 typedef struct{
-	int x,y,z,speed;
+	float x,y,z,speed,acelerationX,acelerationY;
 }Stars;
 
 static void ChronoShow ( char* name, int computations)
@@ -44,21 +44,44 @@ static void FramerateLimit (int max_fps)
   frame_time = GetMsTime();
 }
 
-static void CreaterStars(Stars* st, int max_stars){
+static void CreaterStarsRT(Stars* st, int max_stars,float projection){
 	for(int i = 0; i < max_stars; i++){
-		st[i].x=(rand()%2000) - 1000;
-		st[i].y=(rand()%2000) - 1000;
+		st[i].x=(rand()%1000);
+		st[i].y=(rand()%1000);
 		st[i].z=(rand()%2000);
-		st[i].speed=(rand()%200)+10;
+		st[i].speed=1;
+	}
+}
+static void CreaterStarsRD(Stars* st, int max_stars,float projection){
+	for(int i = 0; i < max_stars; i++){
+		st[i].x=(rand()%1000);
+		st[i].y=(rand()%1000);
+		st[i].z=(rand()%2000);
+		st[i].z=(rand()%2000);
+		st[i].speed=1;
+	}
+}
+static void CreaterStarsLT(Stars* st, int max_stars,float projection){
+	for(int i = 0; i < max_stars; i++){
+		st[i].x=(rand()%1000);
+		st[i].y=(rand()%1000);
+		st[i].z=(rand()%2000);
+		st[i].speed=1;
+	}
+}
+static void CreaterStarsLD(Stars* st, int max_stars,float projection){
+	for(int i = 0; i < max_stars; i++){
+		st[i].x=(rand()%1000);
+		st[i].y=(rand()%1000);
+		st[i].z=(rand()%2000);
+		st[i].z=(rand()%2000);
+		st[i].speed=1;
 	}
 }
 
-// ---------------------------------------------------------------------------
-
-// Fully UNoptimized graphics effect example
-static int DoEffect (short* drawlist, int max_vertices, int w, int h,Stars* st, float projection)
-{
+static int StarsRT(short* drawlist, int max_vertices, int w, int h,Stars* st, float projection,int indice){
   short* ori = drawlist;
+  drawlist+=indice;
   int j;
   int cx = w >> 1;  // Screen center
   int cy = h >> 1;
@@ -67,20 +90,22 @@ static int DoEffect (short* drawlist, int max_vertices, int w, int h,Stars* st, 
       float y = st[j].y;
       float z = st[j].z;
       z -= st[j].speed;
-      st[j].z=z;
       int xp = 0;
       int yp = 0;
-      if(z>0){
-      	xp = cx + (int)((x * projection) / z);
-      	yp = cy - (int)((y * projection) / z);
-      }else{
-      	st[j].x=(rand()%2000) - 1000;
-		st[j].y=(rand()%2000) - 1000;
-		st[j].z=(rand()%2000);
-		st[j].speed=(rand()%200)+10; 
+      if(z<=0){
+		z=2000;
       }
       
-      if ((xp >= 0) && (yp >= 0) && (xp < w) && (yp < h)) {
+      st[j].z=z;
+      
+      float zrec = 1/z;
+      float ProZrec = projection*zrec;
+      
+	  xp = cx + (int)(x * ProZrec);
+      yp = cy - (int)(y * ProZrec);
+        
+      
+      if ((yp >= 0) && (xp < w)) {
         drawlist[0] = xp;
         drawlist[1] = yp;
         drawlist[2] = 0xff;
@@ -90,7 +115,139 @@ static int DoEffect (short* drawlist, int max_vertices, int w, int h,Stars* st, 
 
   // Devuelve el numero de vertices que se han metido en la lista de pintado
   // No es necesario hacerlo con resta de punteros, solo importa devolver el numero correcto
-  return (drawlist - ori) / 3;
+  return drawlist-ori;
+}
+
+static int StarsRD(short* drawlist, int max_vertices, int w, int h,Stars* st, float projection,int indice){
+  short* ori = drawlist;
+  drawlist+=indice;
+  int j;
+  int cx = w >> 1;  // Screen center
+  int cy = h >> 1;
+  for(j = 0; j < max_vertices; j++) {
+      float x = st[j].x;
+      float y = st[j].y;
+      float z = st[j].z;
+      z -= st[j].speed;
+      int xp = 0;
+      int yp = 0;
+      if(z<=0){
+		z=2000;
+      }
+      
+      st[j].z=z;
+      
+      float zrec = 1/z;
+      float ProZrec = projection*zrec;
+      
+	  xp = cx + (int)(x * ProZrec);
+      yp = cy + (int)(y * ProZrec);     
+      
+      if ((yp < h) && (xp < w)) {
+        drawlist[0] = xp;
+        drawlist[1] = yp;
+        drawlist[2] = 0xff;
+        drawlist += 3;
+      } 
+  }
+
+  // Devuelve el numero de vertices que se han metido en la lista de pintado
+  // No es necesario hacerlo con resta de punteros, solo importa devolver el numero correcto
+  return drawlist-ori;
+}
+
+static int StarsLD(short* drawlist, int max_vertices, int w, int h,Stars* st, float projection,int indice){
+	 short* ori = drawlist;
+	 drawlist+=indice;
+  int j;
+  int cx = w >> 1;  // Screen center
+  int cy = h >> 1;
+  for(j = 0; j < max_vertices; j++) {
+      float x = st[j].x;
+      float y = st[j].y;
+      float z = st[j].z;
+      z -= st[j].speed;
+      int xp = 0;
+      int yp = 0;
+      if(z<=0){
+		z=2000;
+      }
+      
+      st[j].z=z;
+      
+      float zrec = 1/z;
+      float ProZrec = projection*zrec;
+      
+	  xp = cx - (int)(x * ProZrec);
+      yp = cy + (int)(y * ProZrec);      
+      
+      if ((yp < h) && (xp >= 0)) {
+        drawlist[0] = xp;
+        drawlist[1] = yp;
+        drawlist[2] = 0xff;
+        drawlist += 3;
+      } 
+  }
+
+  // Devuelve el numero de vertices que se han metido en la lista de pintado
+  // No es necesario hacerlo con resta de punteros, solo importa devolver el numero correcto
+  return drawlist-ori;
+}
+
+static int StarsLT(short* drawlist, int max_vertices, int w, int h,Stars* st,float projection,int indice){
+	 short* ori = drawlist;
+	 drawlist+=indice;
+  int j;
+  int cx = w >> 1;  // Screen center
+  int cy = h >> 1;
+  for(j = 0; j < max_vertices; j++) {
+      float x = st[j].x;
+      float y = st[j].y;
+      float z = st[j].z;
+      z -= st[j].speed;
+      int xp = 0;
+      int yp = 0;
+      if(z<=0){
+		z=2000;
+      }
+      
+      st[j].z=z;
+      
+      float zrec = 1/z;
+      float ProZrec = projection*zrec;
+      
+	  xp = cx - (int)(x * ProZrec);
+      yp = cy - (int)(y * ProZrec);      
+      
+      if ((yp >= 0) && (xp >= 0)) {
+        drawlist[0] = xp;
+        drawlist[1] = yp;
+        drawlist[2] = 0xff;
+        drawlist += 3;
+      } 
+  }
+
+  // Devuelve el numero de vertices que se han metido en la lista de pintado
+  // No es necesario hacerlo con resta de punteros, solo importa devolver el numero correcto
+  return drawlist-ori;
+ }
+
+// ---------------------------------------------------------------------------
+
+// Fully UNoptimized graphics effect example
+static int DoEffect (short* drawlist, int max_vertices, int w, int h,Stars* stRD,Stars* stRT,Stars* stLD,Stars* stLT, float projection)
+{
+  int index=0;
+  
+  index=StarsRD(drawlist,max_vertices,w,h,stRD,projection,index);
+  printf("Index:%d\n",index);
+  index=StarsRT(drawlist,max_vertices,w,h,stRT,projection,index);
+  printf("Index:%d\n",index);
+  index=StarsLD(drawlist,max_vertices,w,h,stLD,projection,index);
+  printf("Index:%d\n",index);
+  index=StarsLT(drawlist,max_vertices,w,h,stLT,projection,index);
+  printf("Index:%d\n",index);
+  return index/3;
 }
 
 // ---------------------------------------------------------------------------
@@ -100,7 +257,9 @@ static void DisplayVertices (unsigned int* pixels, short* drawlist, int n_vertic
   int i;
   for(i = 0; i < n_vertices; i++) {
       int xp = drawlist[0];
+      //printf("XP:%d\n",xp);
       int yp = drawlist[1];
+      //printf("YP:%d\n",yp);
       pixels [xp + yp * stride] = palette[drawlist[2]];
       drawlist += 3;
   } 
@@ -115,7 +274,7 @@ int main ( int argc, char** argv)
 {
   srand(time(NULL));
   int end = 0;
-  int mouse_x = 0, mouse_y = 0;
+  //int mouse_x = 0, mouse_y = 0;
   SDL_Surface  *g_SDLSrf;
   int req_w = 1024;
   int req_h = 768;
@@ -146,15 +305,21 @@ int main ( int argc, char** argv)
   float half_scr_w = (float)(req_w >> 1);
   float projection = (1.0f / tan ( hfov * 0.5f)) * half_scr_w;
   
+  // Main loop
+  g_SDLSrf = SDL_GetVideoSurface();
   // Small footprint buffer for vertices draw  
   // we are not using an structure to avoid padding to 8 bytes (instead of current 6)
   int n_vertices = 40000;
-  Stars cosntalacion[n_vertices];
+  Stars cosntalacionRightTop[10000];
+  Stars cosntalacionRightDown[10000];
+  Stars cosntalacionLeftTop[10000];
+  Stars cosntalacionLeftDown[10000];
   short* drawlist = (short*) malloc (n_vertices * 3 * sizeof(short));
-  CreaterStars(cosntalacion, n_vertices);
-
-  // Main loop
-  g_SDLSrf = SDL_GetVideoSurface();
+  CreaterStarsRT(cosntalacionRightTop, n_vertices/4,projection);
+  CreaterStarsRD(cosntalacionRightDown, n_vertices/4,projection);
+  CreaterStarsLT(cosntalacionLeftTop, n_vertices/4,projection);
+  CreaterStarsLD(cosntalacionLeftDown, n_vertices/4,projection);
+  
   while ( !end) { 
 
     SDL_Event event;
@@ -162,9 +327,9 @@ int main ( int argc, char** argv)
     // Your gfx effect goes here
 
     ChronoWatchReset();
-    int n_draw = DoEffect (drawlist, n_vertices, g_SDLSrf->w, g_SDLSrf->h,cosntalacion,projection);
+    int n_draw = DoEffect (drawlist, n_vertices/4, g_SDLSrf->w, g_SDLSrf->h,cosntalacionRightDown,cosntalacionRightTop,cosntalacionLeftDown,cosntalacionLeftTop,projection);
     assert(n_draw <= n_vertices);
-    ChronoShow ( "Donut festival", n_draw);
+    ChronoShow ( "Stars festival", n_vertices);
 
     // Draw vertices; don't modify this section
     // Lock screen to get access to the memory array
@@ -172,21 +337,21 @@ int main ( int argc, char** argv)
 
     // Clean the screen
     SDL_FillRect(g_SDLSrf, NULL, SDL_MapRGB(g_SDLSrf->format, 0, 0, 0));
-    ChronoShow ( "Clean", g_SDLSrf->w * g_SDLSrf->h);
+    //ChronoShow ( "Clean", g_SDLSrf->w * g_SDLSrf->h);
 
     // Paint vertices
     DisplayVertices (g_SDLSrf->pixels, drawlist, n_draw, g_SDLSrf->pitch >> 2, palette);
-    ChronoShow ( "Preview", n_draw);
+    //ChronoShow ( "Preview", n_draw);
 
     //Unlock the draw surface, dump to physical screen
     ChronoWatchReset();
     SDL_UnlockSurface( g_SDLSrf);
     SDL_Flip( g_SDLSrf);
-    ChronoShow ( "Screen dump", g_SDLSrf->w * g_SDLSrf->h);
+    //ChronoShow ( "Screen dump", g_SDLSrf->w * g_SDLSrf->h);
 
     // Limit framerate and return any remaining time to the OS
     // Comment this line for benchmarking
-    FramerateLimit (60);
+    //FramerateLimit (60);
 
     dump++;
 
@@ -195,14 +360,6 @@ int main ( int argc, char** argv)
     {
       switch (event.type) 
       {
-        case SDL_MOUSEMOTION:
-          mouse_x = event.motion.x;
-          mouse_y = event.motion.y;
-          break;
-        case SDL_MOUSEBUTTONDOWN:
-          //printf("Mouse button %d pressed at (%d,%d)\n",
-          //       event.button.button, event.button.x, event.button.y);
-          break;
         case SDL_QUIT:
           end = 1;
           break;
